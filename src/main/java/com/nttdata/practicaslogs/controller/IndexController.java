8446aabb147e9fe3f8f5b148aa390b879188a64e
Esmeralda Enriquez <78413520+kimmurielO@users.noreply.github.com>
2021-12-01 13:53:55 +0100

    Add files via upload

--- src/main/java/com/nttdata/practicaslogs/controller/IndexController.java
+++ src/main/java/com/nttdata/practicaslogs/controller/IndexController.java
@@ -3,7 +3,9 @@ package com.nttdata.practicaslogs.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.micrometer.core.instrument.Counter;
@@ -16,6 +18,9 @@ public class IndexController {
 	@Autowired
 	private Temperatura temperatura;
 	
+	@Value("${some.valueFar}")
+	private String gradoTest;
+	
 	private final static Logger logger= LoggerFactory.getLogger(IndexController.class);
 	
 	private Counter counterConsulta;
@@ -24,8 +29,19 @@ public class IndexController {
 		this.counterConsulta = Counter.builder("Invocaciones.contadorConsultaTemp").description("Invocaciones totales").register(registry);
 	}
 	
+	@GetMapping(path="/convertirTemp")
+	public String convertirTemp(@RequestParam float tempe) {
+		counterConsulta.increment();
+		logger.info("Se ha llamado a consulta "+counterConsulta.count()+" veces");
+		if(gradoTest.equals("Celsius")) {
+			return ""+temperatura.convertirTemperaturaCaF(tempe);
+		} else {
+			return ""+temperatura.convertirTemperaturaFaC(tempe);
+		}
+	}
+	
 	@GetMapping(path="/consultaTemp")
-	public String consultaTemp() {
+	public String consultaTemp(@RequestParam float tempe) {
 		counterConsulta.increment();
 		logger.info("Se ha llamado a consulta "+counterConsulta.count()+" veces");
 		return ""+temperatura.mensajeTemperatura();
